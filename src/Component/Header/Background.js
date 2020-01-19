60d5cdf84087ff6ae4f6e7c8b44c240a4fc2888b
Hawk Nguyen <verynoob189@gmail.com>
2020-01-19 07:39:06 -0500

    update background's style

--- src/Component/Header/Background.js
+++ src/Component/Header/Background.js
@@ -4,19 +4,23 @@ import "./Background.css";
 const Background = props => {
   return (
     <div className={props.class} data-interval={props.sliderData.intervalTime}>
-      <div className="main-background w-100 d-flex">
-        <div className="d-flex justify-content-between align-items-center p-4 w-100 container">
-          <div class="header-content">
+      <div className="main-background container-fluid  justify-content-center align-items-center">
+        <div className="row justify-content-between align-items-center p-4 w-100 container">
+          <div class="header-content col-sm-6">
             <h4 class="header-title text-left">{props.sliderData.title}</h4>
             <h1 class="header-heading">{props.sliderData.heading}</h1>
-            <span class="header-caption text-left">{props.sliderData.caption}</span>
+            <span class="header-caption text-left">
+              {props.sliderData.caption}
+            </span>
           </div>
-          <div className="slider-content"></div>
-          <img
-            class="w-75"
-            src={require("../../media/" + props.sliderData.url)}
-            alt=""
-          />
+          {/* <div className="slider-content"></div> */}
+          <div className="header-picture col-sm-6">
+            <img
+              class="img-fluid"
+              src={require("../../media/" + props.sliderData.url)}
+              alt=""
+            />
+          </div>
         </div>
       </div>
     </div>
