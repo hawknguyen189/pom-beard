a6daf8a7a135b929453bf4ce00758d5c87d42f76
Hawk Nguyen <verynoob189@gmail.com>
2020-02-12 17:23:38 -0500

    finish contact

--- src/Component/Footer/Contact.js
+++ src/Component/Footer/Contact.js
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div className="col-sm contact-div">
       <div className="contact-info">
-        <h3>CONTACT</h3>
+        <h3 className="footer-header mb-4">Contact</h3>
         <p>
           <i className="fas fa-home contact-icon mr-2 text-center"></i>
           Montreal, QC
@@ -19,33 +19,33 @@ const Contact = () => {
         </p>
         <p>
           <i className="fas fa-globe-americas contact-icon mr-2 text-center"></i>
-          www.pombeard.com
+          <a href="www.pombeard.com">www.pombeard.com</a>
         </p>
       </div>
       <div className="contact-network row">
         <a
           href="https://www.facebook.com/"
-          className="rounded-circle network-bg text-center d-inline-block mr-2"
+          className="rounded-circle network-bg text-center d-flex mr-2"
         >
-          <i className="fab fa-facebook-f col-sm align-middle"></i>
+          <i className="fab fa-facebook-f col-sm align-self-center"></i>
         </a>
         <a
           href="https://twitter.com/"
-          className="rounded-circle network-bg text-center d-inline-block mr-2"
+          className="rounded-circle network-bg text-center d-flex mr-2"
         >
-          <i className="fab fa-twitter col-sm"></i>
+          <i className="fab fa-twitter col-sm align-self-center"></i>
         </a>
         <a
           href="https://www.instagram.com/"
-          className="rounded-circle network-bg text-center d-inline-block mr-2"
+          className="rounded-circle network-bg text-center d-flex mr-2"
         >
-          <i className="fab fa-instagram col-sm"></i>
+          <i className="fab fa-instagram col-sm align-self-center"></i>
         </a>
         <a
           href="https://www.pinterest.com/"
-          className="rounded-circle network-bg text-center d-inline-block mr-2"
+          className="rounded-circle network-bg text-center d-flex mr-2"
         >
-          <i className="fab fa-pinterest-p col-sm"></i>
+          <i className="fab fa-pinterest-p col-sm align-self-center"></i>
         </a>
       </div>
     </div>
